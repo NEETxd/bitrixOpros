4d4cd7a11e18eaa39f18e12a6670e179979efc09
Bair <40424643+NEETxd@users.noreply.github.com>
2023-10-12 01:29:54 +0800

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider-container').slick({
+	  infinite: false
+  });
+	$(document).on('submit','form.vote-form',function(){
+alert('Вы удачно проголосовали');
+});
+});
+console.log('da');
+
